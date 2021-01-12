64aec6f71a268493b0e9388bb780ace0bc527a43
Jessica Donzelli <jedonzelli@gmail.com>
2021-01-12 11:40:29 +0100

    modifica CSS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 document.getElementById('prova').innerHTML = 'Heavy Metal - PROVA JS';
 
-document.getElementById('seconda-prova').innerHTML = 'Subtitle';
+document.getElementById('seconda-prova').innerHTML = 'Subtitle - PROVA JS';
